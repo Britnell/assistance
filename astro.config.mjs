63b5aa7062e661287c2e1333f6c7b48576387f93
Britnell <tommy.britnell@gmail.com>
2024-03-30 09:51:53 +0100

    astro add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), vue()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
